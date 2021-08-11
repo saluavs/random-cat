bf51ac2156eb6ae764980450d5a11285302a4503
saluavs <stephaluartev@gmail.com>
2021-08-10 20:45:51 -0400

    version 1.0

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,81 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <section class="light-blue py-5">
+      <h1 class="titulo font-weight-bold">{{ msg }}</h1>
+    </section>
+
+    <section class="pink">
+      <form class="pt-3">
+        <div class="form-group">
+          <label>Título:</label>
+          <input type="text" class="form-control" v-model="titulo" />
+        </div>
+        <div class="form-group">
+          <label for="">Filtro:</label>
+          <select class="form-control" v-model="filtro">
+            <option value="blur">Blur</option>
+            <option value="mono">Mono</option>
+            <option value="sepia">Sepia</option>
+            <option value="negative">Negative</option>
+            <option value="paint">Paint</option>
+            <option value="pixel">Pixel</option>
+          </select>
+        </div>
+        <div class="p-2">
+          <label for="">Color:</label>
+          <select class="py-1" v-model="color">
+            <option value="red">Rojo</option>
+            <option value="blue">Azul</option>
+            <option value="green">Verde</option>
+            <option value="orange">Naranjo</option>
+            <option value="white">Blanco</option>
+            <option value="yellow">Amarillo</option>
+          </select>
+          <div class="circle mx-3 py-2" :style="{background: this.color}"></div>
+        </div>
+        <div class="form-group">
+          <label>Tamaño:</label>
+          <input 
+            type="number"
+            v-model="tamano"
+            @keyup.enter="mostrarImg"
+            placeholder="200,300,400,etc"
+          />
+        </div>
+        <button type="button" class="btn btn-success" @click="mostrarImg">
+          Obtener mi gatito
+        </button>
+      </form>
+    </section>
+
+    <section class="light-blue py-5" id="image">
+      <div class="mb-5">
+        <img :src="urlImage" alt="" />
+      </div>
+    </section>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data() {
+    return {
+      msg: "Random Gif Cat",
+      urlImage: null,
+      titulo: null,
+      color: null,
+      filtro: null,
+      tamano: null,
+    };
+  },
+  methods: {
+    mostrarImg() {
+      this.urlImage = `https://cataas.com/cat/gif/says/${this.titulo}?filter=${this.filtro}&color=${this.color}&size=${this.tamano}`;
+      console.log("url", this.urlImage);
+    },
+  },
+};
 </script>
 
 <style>
@@ -20,7 +84,28 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+.light-blue {
+  background-color: lightblue;
+}
+.circle{
+  border-radius: 50%;
+  height: 30px;
+  width: 30px;
+  display: inline-block;
+  vertical-align: middle;
+}
+.pink {
+  background-color: #faa7b5;
+}
+body {
+  width: 600px;
+  margin: 0 auto;
+  padding: 20px 20px 20px 20px;
+}
+.titulo{
+  height: 60px;
+  padding-top: 50px;
+  margin: 0 auto;
 }
 </style>
